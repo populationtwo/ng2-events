08e631eb93354c8f008c8485e1de63573a87b6bf
Edwin Lunandy <elunandy@newsignature.com>
2017-07-25 11:34:33 -0400

    Add toastr service.

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core'
-import {EventService} from "./shared/event.service"
+import {EventService} from './shared/event.service'
+import {ToastrService} from '../common/toastr.service'
 
 @Component({
     selector: 'events-list',
@@ -9,7 +10,7 @@ import {EventService} from "./shared/event.service"
 			<hr />
 			<div class="row">
 				<div class="col-md-5" *ngFor="let event of events">
-					<event-thumbnail [event]="event"></event-thumbnail>
+					<event-thumbnail [event]="event" (click)="handleThumbnailClick(event.name)"></event-thumbnail>
 				</div>
 			</div>
 		</div>`
@@ -18,10 +19,14 @@ import {EventService} from "./shared/event.service"
 export class EventsListComponent implements OnInit {
     events: any[]
 
-    constructor(private eventService: EventService) {
+    constructor(private eventService: EventService, private toastr: ToastrService) {
     }
 
     ngOnInit() {
         this.events = this.eventService.getEvents()
+    }
+
+    handleThumbnailClick(eventName) {
+        this.toastr.info(eventName)
     }
 }
