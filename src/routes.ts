fc1e28bf7f7b5acb82fb531e35e39c8e0cb641d6
populationtwo <populationtwo@gmail.com>
2019-05-22 13:53:19 -0400

    Add new session form.

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import {
     EventsListComponent,
     EventDetailsComponent,
     CreateEventComponent,
+    CreateSessionComponent,
     EventRouteActivator,
     EventListResolver
 } from './app/events/index';
@@ -13,6 +14,7 @@ export const appRoutes: Routes = [
     {path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent']},
     {path: 'events', component: EventsListComponent, resolve: {events: EventListResolver}},
     {path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
+    {path: 'events/session/new', component: CreateSessionComponent },
     {path: '404', component: Error404Component},
     {path: '', redirectTo: '/events', pathMatch: 'full'},
     {path: 'user', loadChildren: './user/user.module#UserModule'}
