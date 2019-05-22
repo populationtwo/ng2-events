06ba3848600905e80d4ed5c0dc20d7571927aacd
populationtwo <populationtwo@gmail.com>
2019-05-21 22:42:53 -0400

    Add create event form.

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,30 +1,35 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {EventService} from './shared';
 
 @Component({
-    selector: 'name',
-    template:`
-    <h1>New Event</h1>
-        <hr>
-        <div class="col-md-6">
-        <h3>[Create Event Form]</h3>
-        <br><br>
-        <button class="btn btn-primary" type="submit">Save</button>
-        <button class="btn btn-secondary" type="button" (click)="cancel()">Cancel</button>
-        </div>
-    `
-
+    templateUrl: './create-event.component.html',
+    styles: [`
+    em { float: right; color: #E05C65; padding-left: 10px;}
+        .error input { background-color: #E3C3C5;}
+        .error ::-webkit-input-placeholder { color: #999; }
+        .error ::-moz-placeholder { color: #999; }
+        .error :-moz-placeholder { color: #999; }
+        .error : ms-input-placeholder { color: #999; }
+    `]
 })
 
 export class CreateEventComponent implements OnInit {
+    newEvent;
     isDirty:boolean = true;
-    constructor(private router: Router) {
+    constructor(private router: Router, private eventService: EventService) {
     }
 
     ngOnInit() {
     }
 
     cancel(){
+        this.router.navigate(['/events']);
+    }
+
+    saveEvent(formValues){
+        this.isDirty = false;
+        this.eventService.saveEvent(formValues);
         this.router.navigate(['/events']);
     }
 }
