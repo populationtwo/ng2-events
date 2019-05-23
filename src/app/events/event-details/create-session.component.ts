95d8fc5b3d80c82eec0d86a976c7fc16bb6d7fb4
populationtwo <populationtwo@gmail.com>
2019-05-23 11:42:03 -0400

    Add create session component.

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {ISession, restrictedWords} from '../shared';
 
 @Component({
+    selector: 'create-session',
     templateUrl: './create-session.component.html',
     styles: [`
     em { float: right; color: #E05C65; padding-left: 10px;}
@@ -15,6 +16,9 @@ import {ISession, restrictedWords} from '../shared';
 })
 
 export class CreateSessionComponent implements OnInit {
+    @Output() saveNewSession = new EventEmitter();
+    @Output() cancelAddSession = new EventEmitter();
+
     newSessionForm: FormGroup;
     name: FormControl;
     presenter: FormControl;
@@ -49,8 +53,12 @@ export class CreateSessionComponent implements OnInit {
             voters: []
         };
         console.log(session);
+        this.saveNewSession.emit(session);
     }
 
+    cancel() {
+        this.cancelAddSession.emit()
+    }
 
 }
 
