f9b20e5cb9e286cc1d6e589449c1f3087f5e9281
populationtwo <populationtwo@gmail.com>
2019-05-23 22:52:59 -0400

    Add content projection to session list component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import {Error404Component} from './errors/404.component';
 import {AuthService} from './user/auth.service';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SessionListComponent} from './events/event-details/session-list.component';
+import {CollapsibleWellComponent} from './common/collapsible-well.component';
 
 @NgModule({
   imports: [
@@ -38,7 +39,8 @@ import {SessionListComponent} from './events/event-details/session-list.componen
     CreateEventComponent,
     Error404Component,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    CollapsibleWellComponent
 
   ],
   providers: [
---
+++ src/app/common/collapsible-well.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+    selector: 'collapsible-well',
+    template: `
+        <div class="well pointable" (click)="toggleContent()">
+            <h4><ng-content select="[well-title]"></ng-content></h4>
+            <ng-content *ngIf="visible" select="[well-body]"></ng-content>
+        </div>
+    `
+})
+
+export class CollapsibleWellComponent   {
+
+    @Input() title: string;
+    visible: boolean = true;
+
+    toggleContent(){
+        this.visible = !this.visible;
+    }
+
+}
