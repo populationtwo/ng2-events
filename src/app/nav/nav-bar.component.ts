7a42875ea1551d3eaa5d370ad9f9f34088b7f1a5
populationtwo <populationtwo@gmail.com>
2019-05-29 16:46:33 -0400

    Implement session search.

--- src/app/nav/nav-bar.component.ts
+++ src/app/nav/nav-bar.component.ts
@@ -1,19 +1,40 @@
 import { Component } from "@angular/core";
-import {AuthService} from '../user/auth.service';
+import { AuthService } from "../user/auth.service";
+import { EventService, ISession } from "../events/shared";
 
 @Component({
-  selector: 'nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styles: [`
-    .nav.navbar-nav {font-size: 15px;}
-    #searchForm {margin-right: 100px;}
-    @media (max-width: 1200px) {
-      #searchForm{display:none}
-    }
-      li > a.active { color: #F97924;}
-  `]
+  selector: "nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styles: [
+    `
+      .nav.navbar-nav {
+        font-size: 15px;
+      }
+      #searchForm {
+        margin-right: 100px;
+      }
+      @media (max-width: 1200px) {
+        #searchForm {
+          display: none;
+        }
+      }
+      li > a.active {
+        color: #f97924;
+      }
+    `
+  ]
 })
-
 export class NavBarComponent {
-  constructor(public auth: AuthService) {}
+  searchTerm: string = "";
+  foundSessions: ISession[];
+
+  constructor(private auth: AuthService, private eventService: EventService) {}
+
+  searchSessions(searchTerm) {
+    this.eventService.searchSessions(searchTerm).subscribe(sessions => {
+      this.foundSessions = sessions;
+      console.log(this.foundSessions);
+
+    });
+  }
 }
